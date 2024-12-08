d9b121478ca29df52a6557dd5637bfc94d399bf7
Khun Myo Myat <hanmyo551@gmail.com>
2024-12-08 22:54:50 +0700

    start to write js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const generateBt = document.querySelector(".generate-bt");
+
+generateBt.addEventListener("click", function () {
+  // generate pw
+  // get display pw place
+  // display password
+});
+
+function generatePassword() {}

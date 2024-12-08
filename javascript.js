1c5ac74b4d96b71dc3a50c27bda70ab0c7a91658
Khun Myo Myat <hanmyo551@gmail.com>
2024-12-08 23:30:57 +0700

    write js finish till question about include uppercase letters etc..

--- javascript.js
+++ javascript.js
@@ -2,8 +2,23 @@ const generateBt = document.querySelector(".generate-bt");
 
 generateBt.addEventListener("click", function () {
   // generate pw
+  generatePassword();
   // get display pw place
   // display password
 });
 
-function generatePassword() {}
+function generatePassword() {
+  let passwordLength = parseInt(
+    prompt("Enter password length (between 8 and 128):")
+  );
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid input. Please enter a number between 8 and 128");
+    return "";
+  }
+
+  let inCludeUppercase = confirm("Include Uppercase letters?");
+  let inCludeLowercase = confirm("Include Lowercase letters?");
+  let inCludeNumber = confirm("Include Numbers?");
+  let inCludeSpecialCharacter = confirm("Include Special Characters?");
+}

07cf0025b646dc91d57c52f453819b53d2c65fb6
Khun Myo Myat <hanmyo551@gmail.com>
2024-12-28 09:26:51 +0700

    try to write availablechars  in object

--- javascript.js
+++ javascript.js
@@ -8,8 +8,32 @@ generateBt.addEventListener("click", function () {
 });
 
 function generatePwProcess() {
+  // Ask the user for the password length
   let passwordQuantityprompt = parseInt(
     prompt("Enter the quantity of password between 8 and 128")
   );
-  console.log(typeof passwordQuantityprompt);
+
+  // Check if the length is valid
+  if (
+    isNaN(passwordQuantityprompt) ||
+    passwordQuantityprompt < 8 ||
+    passwordQuantityprompt > 128
+  ) {
+    alert("Invalid length. Please enter a number between 8 and 128.");
+    return ""; // Stop if the input is invalid
+  }
+
+  // Ask the user what types of characters to include
+  var includeUpperCase = confirm("Include Uppercase letters?");
+  var includeLowerCase = confirm("Include Lowercase letters?");
+  var includeNumbers = confirm("Include Numbers?");
+  var includeSpecialChars = confirm("Include Special Characters?");
+
+  // Define the character groups
+  var charGroups = {
+    upper: "ABCDEFGHIJKLNMOPQRSTUVWXYZ",
+    lower: "abcdefghijklnmopqrstuvwxyz",
+    numbers: "0123456789",
+    special: "!@#$%^&*()_+-=[]{}|;:,.<>?",
+  };
 }
